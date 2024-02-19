90d2a3e4f38691356443b340aef263b942611411
ruti <>
2024-02-19 12:51:34 +0300

    Remove error.c

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 TARGET = ciadpi
 CC ?= gcc
 CFLAGS += -std=c99 -O2 -D_XOPEN_SOURCE=500 
-SOURCES = packets.c main.c conev.c proxy.c desync.c error.c
+SOURCES = packets.c main.c conev.c proxy.c desync.c
 
 all:
 	$(CC) $(CFLAGS) $(SOURCES) -I . -o $(TARGET)
--- error.h
+++ error.h
@@ -1,4 +1,41 @@
-int unie(int e);
-void uniperror(char *str);
-int get_e();
-void uniperror(char *str);
+#include <stdio.h>
+#include <errno.h>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#endif
+
+#ifdef _WIN32
+#define get_e() \
+    unie(WSAGetLastError())
+#else
+#define get_e() \
+    errno
+#endif
+
+#ifdef _WIN32
+#define uniperror(str) \
+    fprintf(stderr, "%s: %d\n", str, WSAGetLastError())
+#else
+#define uniperror(str) \
+    perror(str)
+#endif
+
+inline const int unie(int e)
+{
+    #ifdef _WIN32
+    switch (e) {
+        case WSAEWOULDBLOCK:
+            return EAGAIN;
+        case WSAETIMEDOUT:
+            return ETIMEDOUT;
+        case WSAENETUNREACH:
+            return ENETUNREACH;
+        case WSAEHOSTUNREACH:
+            return EHOSTUNREACH;
+        case WSAECONNREFUSED:
+            return ECONNREFUSED;
+    }
+    #endif
+    return e;
+}
