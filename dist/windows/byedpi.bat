9fb5cfbd633470e7601406495ac1c79dc7609674
ruti <>
2024-08-23 00:22:54 +0300

    Change default options in .bat

--- dist/windows/byedpi.bat
+++ dist/windows/byedpi.bat
@@ -1,4 +1,4 @@
 @echo off
 title ByeDPI
 
-ciadpi.exe --split 1 --disorder 3+s --mod-http=h,d --auto --tlsrec 1+s
+ciadpi.exe --split 1 --disorder 3+s --mod-http=h,d --auto=torst --tlsrec 1+s
--- dist/windows/service_install.bat
+++ dist/windows/service_install.bat
@@ -11,7 +11,7 @@ set svc_name="ByeDPI"
 set svc_desc="Local SOCKS proxy server to bypass DPI (Deep Packet Inspection)."
 
 :: Set up launch args (bypass methods) here.
-set svc_bin="\"%cd%\ciadpi.exe\" --split 1 --disorder 3+s --mod-http=h,d --auto --tlsrec 1+s"
+set svc_bin="\"%cd%\ciadpi.exe\" --split 1 --disorder 3+s --mod-http=h,d --auto=torst --tlsrec 1+s"
 
 sc stop %svc_name%
 sc delete %svc_name%
